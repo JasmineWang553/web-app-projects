d1ec96ca73b3458d23398dfa081770c23b995e2d
JasmineWang553 <jasminewang6026@gmail.com>
2022-08-24 23:57:01 -0600

    upload folder

---
+++ 0824_tracker/script.js
@@ -0,0 +1,37 @@
+//Grab Elements
+var addBtn = document.getElementById('add-entry')
+var table = document.querySelector('tbody')
+var itemCounter = 0
+
+//functions
+function addRow(){ // if already empty row no add new row
+    itemCounter++;
+
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+        <td class="input-item">${itemCounter}</td>
+        <td><input class = "input-date" type="date" name = "date"></td>
+        <td><input class = "input-amt" type="number" name = "amt" placeholder = "0.00"></td>
+        <td><input class = "input-desc" type="text" name = "desc" placeholder = "Describe Expense/Income"></td>
+        <td><button type = "button" class = "delete-entry">Delete</button></td>
+    `;
+
+  table.appendChild(tr);
+}
+
+function deleteRow(event){
+    if (event.target.className === "delete-entry"){
+        table.removeChild(event.target.parentNode.parentNode);
+    }
+    const allItems = document.getElementsByClassName("input-item");
+
+    for (let i = 0; i < allItems.length; i++){
+        allItems[i].innerHTML = i + 1
+    }
+    itemCounter--;
+}
+
+//Events
+addBtn.addEventListener("click", addRow);
+table.addEventListener("click", event => deleteRow(event));
